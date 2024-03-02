b6cc8a30c7e9e79dc618ab964d9ea55187eebdd0
kennedymwaniki <kennedymwaniki089@gmail.com>
2024-03-02 12:11:29 +0300

    error when no city is available

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,8 +1,12 @@
 import styles from "./CityList.module.css";
 import Spinner from "./Spinner";
 import CityItem from "./CityItem";
+import Message from "./Message";
 function CityList({ cities, isLoading }) {
   if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return <Message message="Add your own city by clicking on the map" />;
   return (
     <ul className={styles.cityList}>
       {cities.map((city) => (
